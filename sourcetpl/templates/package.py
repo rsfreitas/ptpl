454dc2e7e292003452cba8b3cacbf9edb11f0ce9
Rodrigo Freitas <rsfreitas@users.noreply.github.com>
2017-01-28 15:36:46 -0200

    Add creation of a package configuration file
    - Adding the creation of a package configuration file, required by the
      build-package script. This file contains all package information, such
      as its name, modules and version.
    
    - Adding template for the build-package script.

--- sourcetpl/templates/package.py
+++ sourcetpl/templates/package.py
@@ -28,7 +28,143 @@ from . import FileTemplate
 
 PREFIX = 'package'
 
-BUILD_PACKAGE = '''
+BUILD_PACKAGE = '''#!/bin/bash
+
+#
+# Description:
+#
+# Author: $FULL_AUTHOR_NAME
+# Created at: $DATE
+# Project: $PROJECT_NAME
+#
+# Copyright (c) $YEAR All rights reserved
+#
+
+package_conf="../../package.conf"
+package_tmp_dir=tmpbuild
+arch=i686
+
+# Compile all internal applications
+compile_applications()
+{
+    local modules=`cfget -C $package_conf package/modules`
+
+    for app in ${modules[@]}; do
+        echo "(cd ../../$app/src && make clean && make)"
+    done
+}
+
+# Prepare the current environment to build the package
+prepare_to_build()
+{
+    rm -f *.deb
+    mkdir -p $package_tmp_dir/{DEBIAN,usr/bin,etc/{cron.d,init.d}}
+}
+
+# Copy all necessary files
+copy_necessary_files()
+{
+    local modules=`cfget -C $package_conf package/modules`
+
+    prepare_to_build
+
+    # Copy all binaries
+    for app in ${modules[@]}; do
+        cp -f ../../$app/bin/$arch/* $package_tmp_dir/usr/bin || \\
+            echo "Error while copying binary from module '$app'."
+    done
+
+    # Copy misc scripts
+    local dest_cron=`basename ../misc/*_cron _cron`
+    cp -f ../misc/*_cron $package_tmp_dir/etc/cron.d/$dest_cron || \\
+        echo "Error copying to file '$dest_cron'."
+
+    local dest_init=`basename ../misc/*_initd _initd`
+    cp -f ../misc/*_initd $package_tmp_dir/etc/init.d/$dest_init || \\
+        echo "Error copying to file '$dest_init'."
+
+    # Copy all debian scripts
+    for script in postinst postrm preinst prerm; do
+        cp -f ../debian/$script $package_tmp_dir/DEBIAN || \\
+            echo "Error copying file '$script'."
+    done
+}
+
+clear_temporary_files()
+{
+    rm -rf $package_tmp_dir
+}
+
+# Build the package
+build_package()
+{
+    # Build all package info, such as version, revision, etc
+    local package=`cfget -C $package_conf package/name`
+    local major=`cfget -C $package_conf version/major`
+    local minor=`cfget -C $package_conf version/minor`
+    local release=`cfget -C $package_conf version/release`
+    local beta_release=`cfget -C $package_conf version/beta`
+    local version=$major.$minor.$release
+
+    local depends=''
+    local maintainer=''
+    local description=''
+
+    # Create CONTROL file
+    cat << CONTROL >> $package_tmp_dir/DEBIAN/control
+Package: $package
+Priority: optional
+Version: $version
+Architecture: $arch
+Depends: $depends
+Maintainer: $maintainer
+Description: $description
+CONTROL
+
+    # Build the package
+    if [ $beta_release == true ]; then
+        beta="-Beta"
+    else
+        beta=""
+    fi
+
+    deb_filename=$package-$version.deb
+    fakeroot dpkg-deb -Zgzip -b $package_tmp_dir $deb_filename
+
+    clear_temporary_files
+}
+
+usage()
+{
+    echo "Usage: ./build-package [OPTIONS]"
+    echo
+    echo "Options"
+    echo -e " -h\\t\\tShows this help screen."
+    echo -e " -a [arch]\\tDefines the package destination architecture."
+    echo
+
+    exit 1
+}
+
+while getopts "ha:" OPTION; do
+    case $OPTION in
+        h)
+            usage
+            ;;
+        a)
+            arch=$OPTARG
+            ;;
+        \?)
+            exit 1
+            ;;
+    esac
+done
+
+compile_applications
+copy_necessary_files
+build_package
+
+exit 0
 '''
 
 CLEAN_PACKAGE = '''#!/bin/bash
@@ -36,10 +172,10 @@ CLEAN_PACKAGE = '''#!/bin/bash
 package_dir=../../
 
 # Remove older versions
-rm -rf *.pkg
+rm -rf *.deb
 
 for arq in $package_dir*/src; do
-    echo "Cleaning source directory: <$arq>\n"
+    echo "Cleaning source directory: $arq"
     (cd $arq && make clean)
 done
 
@@ -117,6 +253,21 @@ esac
 exit 0
 '''
 
+PACKAGE_CONF = '''# Main package informations.
+# The package modules must be separated by spaces.
+[package]
+name=$PROJECT_NAME
+modules=$PROJECT_NAME
+
+# Package version: major.minor.release
+# Example: 0.1.1
+[version]
+major=0
+minor=1
+release=1
+beta=true
+'''
+
 class Package(object):
     def __init__(self, args, project_vars):
         self._args = args
@@ -167,7 +318,11 @@ class Package(object):
 
             # initd
             (prefix + '_initd', True, 'misc',
-                Template(INITD).safe_substitute(self._project_vars))
+                Template(INITD).safe_substitute(self._project_vars)),
+
+            # package.conf
+            ('package.conf', False, '',
+                Template(PACKAGE_CONF).safe_substitute(self._project_vars))
         ]
 
         for script in files:
@@ -195,8 +350,12 @@ class Package(object):
     def _create_files(self):
         for filename in self._files.filenames():
             file_data = self._files.properties(filename)
-            pathname = self._root_dir + '/package/' + file_data.get('path') + \
-                    '/' + filename
+            path = file_data.get('path')
+
+            if len(path):
+                pathname = self._root_dir + '/package/' + path + '/' + filename
+            else:
+                pathname = self._root_dir + '/' + filename
 
             with open(pathname, 'w') as out_fd:
                 out_fd.write(file_data.get('data'))
